ada20006c025356a6f723ed3254076c342ebb551
Saliq Tariq <56510866+saliqtariq@users.noreply.github.com>
2025-09-10 00:14:58 -0700

    Add old route

--- server/old.js
+++ server/old.js
@@ -39,3 +39,98 @@ app.use("/books", bookRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log("Server running at port:", PORT));
+
+
+
+import express from "express";
+import { getBooks, storeBook, updateBook, deleteBook } from "../mongoose.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     responses:
+ *       200:
+ *         description: List of books
+ */
+router.get("/", async (req, res) => {
+  const books = await getBooks();
+  res.json(books);
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: Add a new book
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Book added
+ */
+router.post("/", async (req, res) => {
+  const { title, author } = req.body;
+  await storeBook({ title, author });
+  res.json({ success: true });
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   put:
+ *     summary: Update a book
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     responses:
+ *       200:
+ *         description: Book updated
+ */
+router.put("/:id", async (req, res) => {
+  await updateBook(req.params.id, req.body);
+  res.json({ success: true });
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   delete:
+ *     summary: Delete a book
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book deleted
+ */
+router.delete("/:id", async (req, res) => {
+  await deleteBook(req.params.id);
+  res.json({ success: true });
+});
+
+export default router;
