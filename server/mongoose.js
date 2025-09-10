76c7dd6c6ef8e6be1e539037f1f59101530edce6
mooojn <moojntariq@gmail.com>
2025-09-10 01:38:48 -0700

    Fix mongoose

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,29 +1,35 @@
 import mongoose from "mongoose";
-import { book } from './models/book.js';
+import { book } from "./models/book.js";
 
 // const URI = 'mongodb://localhost:27017/Library';
-const URI = 'mongodb+srv://moojntariq_db_user:5H4oY48aZsjCTiSA@moon.yk5hjzg.mongodb.net/Library?retryWrites=true&w=majority&appName=moon';
+const URI =
+  "mongodb+srv://moojntariq_db_user:5H4oY48aZsjCTiSA@moon.yk5hjzg.mongodb.net/Library?retryWrites=true&w=majority&appName=moon";
 
 // connect once
-mongoose.connect(URI)
+mongoose
+  .connect(URI)
   .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ Connection error:", err));
+  .catch((err) => console.error("❌ Connection error:", err));
 
 export async function getBooks() {
   return await book.find();
 }
 
 export async function storeBook(json) {
-  await book.insertOne({title:json.title, author:json.author});
+  return await book.create({ title: json.title, author: json.author });
 }
 
 export async function updateBook(id, json) {
-  await book.updateOne({_id: id},{
-    title:json.title,
-    author:json.author
-  })
+  return await book.findByIdAndUpdate(
+    id,
+    {
+      title: json.title,
+      author: json.author,
+    },
+    { new: true } // returns updated doc
+  );
 }
 
 export async function deleteBook(id) {
-  await book.deleteOne({_id: id})
+  return await book.findByIdAndDelete(id);
 }
