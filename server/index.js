800aa1b972e8a861e4f6325e255a892bd52146e4
mooojn <moojntariq@gmail.com>
2025-09-10 01:34:53 -0700

    Link server with vercel

--- server/index.js
+++ server/index.js
@@ -6,13 +6,10 @@ const app = express();
 app.use(CORS());
 app.use(express.json());
 
-// Home
 app.get("/", (req, res) => {
   res.send("Running");
 });
 
-// Mount all /books routes
 app.use("/books", bookRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log("Server running at port:", PORT));
+export default app;
